4ee8e459ec485a51659712648b5954bad5b141df
noracurcio <noracurcio@gmail.com>
2020-11-23 21:49:13 -0700

    Initial file structure.

---
+++ employee-directory/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+// import Employees from "./components/Employees";
+// import Search from "./components/Search";
+
+
+
+function App() {
+  return (
+    <div className = "app">
+      
+    </div>
+  );
+}
+
+export default App;
